0dc0651c9ee81b0c9aeee50f5fa70fac62af7f5a
Roctsk <rostyk20166@gmail.com>
2025-05-05 21:56:35 +0300

    Do the second part of project

--- js/register.js
+++ js/register.js
@@ -3,6 +3,25 @@ const loginButton = document.getElementById("login");
 const container = document.getElementById("container");
 const bgMusic = document.getElementById("bg-music")
 
+const harryImg = document.querySelector('.img-HarryPoter');
+const comment = document.getElementById('harry-comment');
+
+harryImg.addEventListener('animationend', () => {
+  comment.style.display = 'block';
+});
+
+let clickCount = 0;
+harryImg.addEventListener('click', () => {
+  clickCount++;
+  if (clickCount === 1) {
+    comment.textContent = 'Зараз я розкажу як получити підказки!)';
+  } else if (clickCount === 2) {
+    comment.textContent = 'Текст після другого кліку';
+  } else {
+    comment.textContent = 'Удачі з квізами';
+  }
+});
+
 registerButton.addEventListener("click", () => {
   container.classList.add("right-panel-active");
 });
